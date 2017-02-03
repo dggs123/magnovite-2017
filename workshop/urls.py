cc6e3719007c8e104f0051fb9fe8657973bf1399
gaurav <dggs222@gmail.com>
2017-02-03 08:42:39 +0530

    added exel file

--- workshop/urls.py
+++ workshop/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     url(r'^$', index, name='workshop'),
      url('^api/register/(?P<id>\d+)/$', register, name='registerw'),
      url('^api/unregister/(?P<id>\d+)/$', unregister, name='unregisterw'),
-     # url('^exel$', generate_exel, name='wexel'),
+     url('^exel$', generate_exel, name='wexel'),
      # url('^mexel$', generate_exel_invoice, name='mexel')
 ]
