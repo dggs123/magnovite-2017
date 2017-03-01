64b41e4bb31df59ef55de27c290df2e2c2a6373a
gaurav <dggs222@gmail.com>
2017-03-02 02:16:25 +0530

    added code

--- static/home_new/assets/js/main.js
+++ static/home_new/assets/js/main.js
@@ -1,8 +1,4 @@
 
-$('#clock').countdown('2017/3/2', function(event) {
-  	$(this).html(event.strftime('%D days'));
-});
-
 var isMobile = /Android/i.test(navigator.userAgent);
 if (isMobile) {
     window.open(url,'_blank');
